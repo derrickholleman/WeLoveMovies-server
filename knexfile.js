eb209a7db75367aa1f43da7901a51e3b9d599942
Derrick Holleman <derrickholleman@gmail.com>
2021-11-15 09:57:10 -0600

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,5 @@
-const path = require("path");
-
 require("dotenv").config();
+const path = require("path");
 
 const { NODE_ENV = "development", DATABASE_URL } = process.env;
 const URL = NODE_ENV === "production" ? DATABASE_URL : DATABASE_URL;
@@ -9,7 +8,6 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
@@ -17,30 +15,15 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
   },
-
+  
   production: {
     client: "postgresql",
     connection: URL,
-    pool: { min: 0, max: 5 },
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
     },
     seeds: {
       directory: path.join(__dirname, "src", "db", "seeds"),
     },
-  },
-
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: ":memory:",
-    },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
   },
 };
