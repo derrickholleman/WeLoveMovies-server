b24e9344db3af4cb5b51359976a545ae011282bb
Derrick Holleman <derrickholleman@gmail.com>
2021-11-16 09:34:54 -0600

    added method not allowed for reviews routes

--- src/movies/movies.router.js
+++ src/movies/movies.router.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const controller = require("./movies.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/:movieId/theaters").get(controller.readMovieAndTheaters);
+router.route("/:movieId/theaters").get(controller.readMovieAndTheaters).all(methodNotAllowed)
 
-router.route("/:movieId/reviews").get(controller.readMovieAndReviews);
+router.route("/:movieId/reviews").get(controller.readMovieAndReviews).all(methodNotAllowed)
 
 router.route("/:movieId").get(controller.read).all(methodNotAllowed);
 
