fd0d4a912097637151e0e913bf5ea1e5d753038a
Derrick Holleman <derrickholleman@gmail.com>
2021-12-06 14:48:08 -0600

    refactor movies service

--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -1,5 +1,15 @@
 const knex = require("../db/connection");
-const addCritics = require('../utils/addCritics')
+const mapProperties = require('../utils/map-properties')
+
+const addCritics = mapProperties({
+  // keys must match column names in table
+  critic_id: "critic.critic_id",
+  preferred_name: "critic.preferred_name",
+  surname: "critic.surname",
+  organization_name: "critic.organization_name",
+  created_at: "critic.created_at",
+  updated_at: "critic.updated_at"
+});
 
 function list() {
   return knex("movies").select("*");
@@ -31,7 +41,7 @@ function readMovieAndReviews(movie_id) {
     .join("reviews as r", "m.movie_id", "r.movie_id")
     .join("critics as c", "r.critic_id", "c.critic_id")
     .where({ "m.movie_id": movie_id })
-    .then(addCritics)
+    .then((reviews)=>reviews.map(addCritics))
 }
 
 module.exports = {
